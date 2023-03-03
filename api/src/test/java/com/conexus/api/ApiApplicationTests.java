01177166c6b8e4348779e9b18c08758677fc3c4b
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2023-03-03 16:10:59 -0300

    Criação do projeto

---
+++ api/src/test/java/com/conexus/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conexus.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
