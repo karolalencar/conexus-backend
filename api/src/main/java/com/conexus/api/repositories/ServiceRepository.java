d53e7e56da9663b2518936259ce6b2f560313a18
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2023-03-06 16:38:45 -0300

    Arquivos iniciais do repositories adicionados

---
+++ api/src/main/java/com/conexus/api/repositories/ServiceRepository.java
@@ -0,0 +1,8 @@
+package com.conexus.api.repositories;
+
+import com.conexus.api.domain.Service;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ServiceRepository extends CrudRepository<Service, Long> {
+
+}
