d64b079764ca3f53cc964a5d67738f3a6b7b5ccf
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2023-03-09 16:20:53 -0300

    Arquivos iniciais do controllers adicionados. index.html adicionado

---
+++ api/src/main/java/com/conexus/api/controllers/ClientController.java
@@ -0,0 +1,11 @@
+package com.conexus.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/clients")
+public class ClientController {
+
+
+}
---
+++ api/src/main/java/com/conexus/api/controllers/ProfessionalController.java
@@ -0,0 +1,9 @@
+package com.conexus.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("professionals")
+public class ProfessionalController {
+}
