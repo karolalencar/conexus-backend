623cead5cf2d613e3c17219856e81266797efd14
Karulina Alencar de Freitas <karulina.freitas@opus-software.com.br>
2023-03-13 17:53:00 -0300

    Iniciando um controller

--- api/src/main/java/com/conexus/api/controllers/ClientController.java
+++ api/src/main/java/com/conexus/api/controllers/ClientController.java
@@ -1,11 +1,29 @@
 package com.conexus.api.controllers;
 
+import com.conexus.api.services.ClientService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import static java.lang.Long.parseLong;
 
 @Controller
 @RequestMapping("/clients")
 public class ClientController {
 
+    private final ClientService clientService;
 
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping({"/{id}/show"})
+    public String showById(@PathVariable String id, Model model) {
+        model.addAttribute("client", clientService.findById(parseLong(id)));
+
+        return "clients/show";
+    }
 }
